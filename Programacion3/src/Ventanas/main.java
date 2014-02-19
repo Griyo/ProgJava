88ba71ba6c9810483d7191ed4a98b1c28b737d78
Griyo <griyovillegas@gmail.com>
2014-02-19 16:58:20 -0700

    ProyectoIniciado
    ProyectoIniciado

---
+++ Programacion3/src/Ventanas/main.java
@@ -0,0 +1,11 @@
+package Ventanas;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		mainWindow g = new mainWindow();
+		
+	}
+
+}
---
+++ Programacion3/src/Ventanas/mainWindow.java
@@ -0,0 +1,12 @@
+package Ventanas;
+
+import javax.swing.JFrame;
+
+public class mainWindow extends JFrame {
+	
+	public mainWindow (){
+		this.setTitle("Griyo's Windows");
+		this.setSize(400, 500);
+		this.setVisible(true);
+	}
+}
