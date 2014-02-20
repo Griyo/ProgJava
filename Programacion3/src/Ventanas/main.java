18d9c1e7fd6b3abde77e5053626d85a76ddc98f7
Griyo <griyovillegas@gmail.com>
2014-02-19 17:42:26 -0700

    Boton
    Primer Boton de ventana

--- Programacion3/src/Ventanas/main.java
+++ Programacion3/src/Ventanas/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		mainWindow g = new mainWindow();
+		mainWindow g = new mainWindow("Griyo's Windows", 400, 500);
 		
 	}
 }
--- Programacion3/src/Ventanas/mainWindow.java
+++ Programacion3/src/Ventanas/mainWindow.java
@@ -1,13 +1,40 @@
 package Ventanas;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class mainWindow extends JFrame {
 	
+	
+	private int altura=0, ancho=0;
+	private String titulo ="";
+	
+	JButton btn;
+	
+	public mainWindow (String titulo, int ancho, int altura){
+		this.setTitle(titulo);
+		this.setSize(ancho, altura);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		btn = new JButton ("Hola Mundo Grafico!");
+		add(btn);
+		this.setVisible(true);
+	}
+
 	public mainWindow (){
 		this.setTitle("Griyo's Windows");
-		this.setSize(400, 500);
-		this.setVisible(true);
+		this.setSize(200, 250);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		btn = new JButton ("Hola Mundo Grafico!");
+		add(btn);
+		this.setVisible(true);
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 }
